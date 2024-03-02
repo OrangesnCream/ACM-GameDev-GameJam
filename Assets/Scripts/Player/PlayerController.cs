7abd17282945ec44bee02e6a71cfb26c302c64a6
unknown <lalvarado.rivas3134@gmail.com>
2024-03-02 14:57:04 -0800

    fixed jittery movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float baseSpeed = 10f;
     public float sprintSpeed = 15f;
     private float speed = 10f;
-
+    public float deceleration=1f;
+    public float acceleration=2f;
     public Rigidbody2D rb;
     Vector2 movement;
+    Vector2 frameVelocity;
     //public GameObject pauseMenu;
     void Start()
     {
@@ -45,10 +46,28 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        //ApplyMovement();
     }
     void Move()
     {
+        //this method was too slow 
+        if(movement.x==0){
+           frameVelocity.x =Mathf.MoveTowards(frameVelocity.x,0,deceleration*Time.fixedDeltaTime);
+        }else{
+            frameVelocity.x= Mathf.MoveTowards(frameVelocity.x,movement.x*speed,acceleration*Time.fixedDeltaTime);
+        }
+        if(movement.y==0){
+          
+           frameVelocity.y =Mathf.MoveTowards(frameVelocity.y,0,deceleration*Time.fixedDeltaTime);
+        }else{
+          
+           frameVelocity.y =Mathf.MoveTowards(frameVelocity.y,movement.y*speed,acceleration*Time.fixedDeltaTime);
+        }
         rb.velocity = movement.normalized * speed;
+    }
+    private void ApplyMovement(){
+        //uncomment in fixed update to run
+        rb.velocity=frameVelocity;
     }
 }
 
